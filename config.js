4cc302ac39edfd3f6c3983bc31889eb5d8acb316
cabreera07 <tu_correo@ejemplo.com>
2026-08-02 15:42:54 -0600

    feat: actualizacion de config y diagnostico

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-const AURASYS_CONFIG = {
+window.AURASYS_CONFIG = {
+  logoPath: "assets/logo/logo-aurasys.svg",
   whatsappUrl: "https://wa.me/50258798301",
-  email: "CABREERA07WALTER@gmail.com",
-  tiktokUrl: "https://www.tiktok.com/@tuusuario",
-  googleScriptUrl: "PEGA_AQUI_TU_URL_DE_GOOGLE_APPS_SCRIPT",
-  logoPath: "assets/icons/icon-aurasys.svg",
-  iconPath: "assets/icons/icon-aurasys.svg"
+  email: "hola@aurasys.gt",
+  tiktokUrl: "https://www.tiktok.com/@aurasys.gt",
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycbxnJsUt7yxARWue2rs-ZCjr_Z9F1Z-04ylMOnlB55jHYTVEToBMeuUqZawvxmxeLY2zPA/exec"
 };
